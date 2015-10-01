6b036173d447e7d629ee910bd22a83a3b5767087
Aaditya Singh <2019asingh@link.csl.tjhsst.edu>
2015-10-01 10:09:30 -0400

    Start programming graph generation. Work on distance calculation.

--- RR_Graph/rom_graph_gen.py
+++ RR_Graph/rom_graph_gen.py
@@ -1,1 +1,39 @@
 # This program generates the graph for the railroads in Romania.
+
+import math
+
+# Make a node hash, with a name corresponding to a 2-tuple of latitude and longitude
+def node_hash():
+  h = {}
+  array_of_lines_from_file = open('rom_nodes.txt').read().split('\n')
+  for l in array_of_lines_from_file:
+    arr = l.split()
+    h[arr[0]] = (float(arr[1]), float(arr[2]))
+
+  return h
+
+# Finds the distance between two nodes
+def dist(n1, n2):
+  n = node_hash()
+  lat1 = n[n1][0] * math.pi/180
+  long1 = n[n1][1] * math.pi/180
+  lat2 = n[n2][0] * math.pi/180
+  long2 = n[n2][1] * math.pi/180
+
+  del_lat = lat2 - lat1
+  del_long = long2 - long1
+
+  a = math.sin()
+
+
+# Makes a hash, where each node corresponds to a list of 2-tuples. Each 2-tuple has the first number referring to another node, and the second number referring to the distance to the node
+def make_graph():
+  g = {}
+  for c in node_hash().keys:
+    g[c] = []
+  lines_from_file = open('rom_edges.txt').read().split('\n')
+  for l in lines_from_file:
+    arr = l.split()
+    g[arr[0]].append(arr[1])
+    g[arr[1]].append(arr[0])
+
